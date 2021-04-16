ad194f5e32279d5e05048958dea7fc93779239d5
Eric_Howland <eric.howland@gmail.com>
2021-04-16 14:38:52 -0500

    update from Quinn

--- MHTML_getter.py
+++ MHTML_getter.py
@@ -17,24 +17,40 @@ pyautogui.FAILSAFE = True
 pyautogui.PAUSE = 1
 
 
+
 # you can install chrome driver from https://chromedriver.storage.googleapis.com/index.html?path=88.0.4324.96/
 driver = webdriver.Chrome()
 
 
+def declare_first_bool(num):
+    #^this directs pyautogui to do the simpler save only on the first file opened in the sequence 
+    #it is switched to 1 after first download 
+    global first_bool
+    first_bool = num    
+    #^may not be needed i really dont know how th desktop save is configured         
+ 
+        
+global table_nums 
+table_nums = [1,2,3]
+#^idk how to declare empty 
+        
+def declare_table_numbers (num1, num2, num3):
+    global table_nums 
+    table_nums[0] = num1
+    table_nums[1] = num2
+    table_nums[2] = num3
+    
+def declare_total_count(x):
+    global total_count
+    total_count = x 
+
+def declare_run_count(x):
+    global run_count
+    run_count = x 
+
+
 def enter_site():
     driver.get("https://wcca.wicourts.gov/advanced.html")
-
-
-def name_enter(last_name, first_name):
-    driver.find_element(By.ID, 'lastName').send_keys(last_name)
-    driver.find_element_by_xpath(
-        'html/body/div[1]/div/div[1]/main/div/form/div[2]/div[3]/div/fieldset/div[1]/div[2]/div/label/input') \
-        .send_keys(first_name)
-    # ^some text entry boxes, like "last name" above, have full word ids but "first name" does not lol
-    pyautogui.hotkey('enter')
-    # ^Probably a better way to do this but I'm Bad
-    # part 2: click first available case
-
 
 def county():
 #Enter County as a search parameter
@@ -87,8 +103,8 @@ def search_results():
     case_nums =  re.findall(r'\d+', case_list_details)
     #^regex to find numerics
     print (case_nums)
-    total_count = 0
-    case_pages(case_nums, start_date)
+    declare_table_numbers(int(case_nums[0]), int(case_nums[1]), int(case_nums[2]))
+                          
     #except:
         #print('straight to single case')
         #captcha_wait()
@@ -99,26 +115,27 @@ def search_results():
             #print('first = ', first_bool)
         #else:
             #awful_save()
-        #next_date(start_date)
- 
-def declare_total_count(something):
-    global total_count
-    total_count = something 
+    #next_date(start_date)
+
     
-def case_pages(case_nums, start_date): 
-    declare_total_count = 0
-    for pages in range (1,math.ceil(int(case_nums[2])/25)+1):
-        print('case results page = ', pages, ' out of ', math.ceil(int(case_nums[2])/25)) 
-        all_cases_on_page(case_nums)
-        driver.find_element(By.LINK_TEXT, 'Next').click() 
-    print('Congrats, all files saved!') 
+def case_pages(): 
+    declare_total_count(0)
+    pages = 1
+    search_results()
+    while pages <= math.ceil(table_nums[2]/25) and total_count <= table_nums[2]:
+        print('case results page = ', pages, ' out of ', math.ceil(table_nums[2] / 25)) 
+        all_cases_on_page()
+        pages = pages + 1
+        driver.find_element(By.LINK_TEXT, 'Next').click()
+        search_results()
+    print('Congrats, all files in date range saved!') 
     declare_first_bool(1)
-    next_date(start_date)
     
     
-def all_cases_on_page(case_nums): 
-    for row_num in range(1, (int(case_nums[1])- int(case_nums[0])+2)):
-        print ('row number = ', row_num)  
+def all_cases_on_page(): 
+    row_num = 1
+    while row_num <= (table_nums[1] - table_nums[0] + 1) and total_count <= table_nums[2]:    
+        print ('row on page = ', row_num, '/', str(table_nums[1] - table_nums[0] + 1))  
         cases_iter = 'html/body/div[1]/div/div[1]/main/div/div[2]/div/table/tbody/tr[' + str(row_num) + ']/td[1]/a' 
         lnks = driver.find_elements(By.XPATH, cases_iter)
     # cant seem to land any closer to the hyperlink than its containing list via XPATh. So this will traverse list find
@@ -131,21 +148,23 @@ def all_cases_on_page(case_nums):
         # ^hopefully this finds case in case selection table             
         captcha_wait()
         open_charge_details()
-        if first_bool == 0:
-            first_save()
-            print('first save')
-            declare_first_bool(1)
-            declare_total_count((total_count+1))
-        else: 
-            awful_save()
-            print('awful save')
-            declare_total_count(total_count+1)
-        print('total count = ', total_count)
+        #if first_bool == 0:
+            #first_save()
+            #print('first save')
+            #declare_first_bool(1)
+            #declare_total_count((total_count+1))
+        #else: 
+        awful_save()
+        print('awful save')
+        row_num = row_num + 1
+        declare_total_count(total_count + 1)
+        declare_run_count(run_count + 1)
+        print('files saved in this run = ', run_count)
+        print('Cases in date range = ', total_count, '/', table_nums[2] )
         return_to_results()              
     
 
 def captcha_wait():
-    
     w2 = WebDriverWait(driver, 600).until(
         EC.presence_of_element_located((By.XPATH, 'html/body/div[1]/div/div[1]/main/div/div/div[2]/div[4]/div/a')))
     
@@ -200,16 +219,18 @@ def main(date_1, date_2):
     county()
     class_codes()
     date_range(date_1, date_2)
-    search_results()
+    case_pages()
+    next_date(date_1)
     
-    
-def declare_first_bool(num):
-    #^this directs pyautogui to do the simpler save only on the first file opened in the sequence 
-    #it is switched to 1 after first download
-    global first_bool
-    first_bool = num    
-    
+
     
 declare_first_bool(0)
-main('10-22-2018', '10-30-2018')
+declare_run_count(0)
+main('1-5-2019', '2-4-2019')
+
+
+
+
+
+
 
